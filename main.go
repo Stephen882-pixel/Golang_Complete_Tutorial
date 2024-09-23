8a55db4d95dc3813a3a9a25bcb640ffcfc7d75ba
Stephen882-pixel <stephenondeyo0@gmail.com>
2024-09-23 18:03:23 +0300

    Integer datatypes

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main(){
 	fmt.Println("The float32 value is:",c)
 	fmt.Println("The string value is:",d)
 
-
+	// Go Boolean datatypes
 	var q bool = true //typed declaration with initial value 
 	var w = false // untyped declaration with initial value 
 	var e bool // typed declaration without initial value
@@ -26,6 +26,16 @@ func main(){
 	fmt.Println("The value of w is:",w)
 	fmt.Println("The value of e is:",e)
 	fmt.Println("The value of r is:",r)
+
+	// GO Integer data types
+			// Integer datatypes are used to store wholes numbers without decimala eg 50,-3500,125000
+			//The integer data types have two categories:Signed Integers(can store both positive and negative values),Unsigned Integer(can only store non-negative numbers)
+
+	
+	var x int = 500
+	var y int = -4500
+	fmt.Printf("Type: %T, value %v",x,x)
+	fmt.Printf("Type: %T, value %v",y,y)
 	
 }
 
